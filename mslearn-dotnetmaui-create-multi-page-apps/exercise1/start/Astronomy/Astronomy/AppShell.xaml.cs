2fe6739ee2e29512b20af5824bb4da0317dddf24
zongo2703 <awazongo42@gmail.com>
2022-12-16 00:04:33 +0000

    MAUI project

---
+++ mslearn-dotnetmaui-create-multi-page-apps/exercise1/start/Astronomy/Astronomy/AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Astronomy;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+
+
+    }
+}
---
+++ mslearn-dotnetmaui-create-user-interface-xaml/exercise1/Notes/SharedResources.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes
+{
+    static class SharedResources
+    {
+        public static readonly Color FontColor = Color.FromRgb(0, 0, 0xFF);
+        public static readonly Color BackgroundColor = Color.FromRgb(0xFF, 0xF0, 0xAD);
+
+    }
+}
